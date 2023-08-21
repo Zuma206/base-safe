3b3d8c5280aa651cfcfafe1bc24b8a68c50efd28
Zuma <james.peedle@yahoo.com>
2023-08-22 00:14:27 +0100

    Added multi-level queries to type

--- src/query.ts
+++ src/query.ts
@@ -1,34 +1,57 @@
 import { ArrayType, RecordType } from "./types";
 
-export type Query<T extends RecordType> = { [K in keyof T]?: T[K] } & {
-  [K in keyof T as T[K] extends number ? `${K & string}?gt` : never]?: number;
+type QueryType<T extends RecordType, P extends string = ""> = {
+  [K in keyof T as `${P}${K & string}`]?: T[K];
 } & {
-  [K in keyof T as T[K] extends number ? `${K & string}?lt` : never]?: number;
+  [K in keyof T as T[K] extends number
+    ? `${P}${K & string}?gt`
+    : never]?: number;
 } & {
-  [K in keyof T as T[K] extends number ? `${K & string}?gte` : never]?: number;
+  [K in keyof T as T[K] extends number
+    ? `${P}${K & string}?lt`
+    : never]?: number;
 } & {
-  [K in keyof T as T[K] extends number ? `${K & string}?lte` : never]?: number;
+  [K in keyof T as T[K] extends number
+    ? `${P}${K & string}?gte`
+    : never]?: number;
 } & {
-  [K in keyof T as T[K] extends string ? `${K & string}?pfx` : never]?: string;
+  [K in keyof T as T[K] extends number
+    ? `${P}${K & string}?lte`
+    : never]?: number;
 } & {
-  [K in keyof T as T[K] extends number ? `${K & string}?r` : never]?: [
+  [K in keyof T as T[K] extends string
+    ? `${P}${K & string}?pfx`
+    : never]?: string;
+} & {
+  [K in keyof T as T[K] extends number ? `${P}${K & string}?r` : never]?: [
     number,
     number
   ];
 } & {
   [K in keyof T as T[K] extends string
-    ? `${K & string}?contains`
+    ? `${P}${K & string}?contains`
     : never]?: string;
 } & {
   [K in keyof T as T[K] extends string
-    ? `${K & string}?not_contains`
+    ? `${P}${K & string}?not_contains`
     : never]?: string;
 } & {
   [K in keyof T as T[K] extends ArrayType
-    ? `${K & string}?contains`
+    ? `${P}${K & string}?contains`
     : never]?: T[K] extends Array<infer U> ? U : never;
 } & {
   [K in keyof T as T[K] extends ArrayType
-    ? `${K & string}?not_contains`
+    ? `${P}${K & string}?not_contains`
     : never]?: T[K] extends Array<infer U> ? U : never;
 };
+
+export type Query<
+  T extends RecordType,
+  Prefix extends string = ""
+> = T extends object
+  ? {
+      [K in keyof T]: T[K] extends RecordType
+        ? Query<T[K], `${Prefix}${K & string}.`>
+        : QueryType<T, Prefix>;
+    }[keyof T]
+  : T;
